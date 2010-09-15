b6914830ed3893be82e6229dbb71ec1285b59a7a
Søren Lund <soren@lund.org>
2010-09-15 15:02:59 +0200

    First working version. Now with tests. README file changed to pod, which doesn't conflict with default dzil rules.

--- lib/Digest/PasswordComposer.pm
+++ lib/Digest/PasswordComposer.pm
@@ -1,7 +1,22 @@
 use strict;
 use warnings;
+
+BEGIN {
+  use Exporter ();
+  our (@ISA, @EXPORT, @EXPORT_OK);
+
+  @ISA = qw(Exporter);
+  @EXPORT = qw(&pwdcomposer);
+  @EXPORT_OK = qw();
+}
+
+our $EXPORT_OK;
+
 package Digest::PasswordComposer;
 # ABSTRACT: Generate unique passwords for web sites.
+
+use Digest::MD5 qw(md5_hex);
+
 
 =head1 SYNOPSIS
 
@@ -9,13 +24,33 @@ This module can generate unique passwords for web sites, using the same algorith
 
 =cut
 
+sub new {
+  my ($class) = @_;
+
+  bless { domain => '' }, $class;
+}
+
 sub pwdcomposer {
-    my ($domain, $pwd) = shift;
-    return "$domain:$pwd";
+  my ($domain, $pwd) = @_;
+  return substr(md5_hex("$pwd:$domain"),0,8);
+}
+
+sub domain {
+  my $self = shift;
+  if (@_) { $self->{domain} = shift }
+  return $self->{domain};
+}
+
+sub password {
+  my $self = shift;
+  return pwdcomposer($self->{domain},shift);
 }
 
 =head1 SEE ALSO
 
 L<http://www.xs4all.nl/~jlpoutre/BoT/Javascript/PasswordComposer>
 
+=cut
+
 1;
+
